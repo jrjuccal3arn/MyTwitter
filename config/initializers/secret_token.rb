7a56489340244ea8c27bcd9910f92fc74ac2a8ee
Junior <junior@l3arn.net>
2012-04-29 18:21:43 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyTwitter::Application.config.secret_token = '77ceb89333b111c56c5ada873fbfe0f47f3d20727fe4cc760711e261dd9216b4f7c4c6a6174140b7e46bf2657633503a267fe25ee07621676eff433260ce265e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MyTwitter::Application.config.session_store :cookie_store, key: '_MyTwitter_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MyTwitter::Application.config.session_store :active_record_store
